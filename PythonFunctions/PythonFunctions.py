f1785e7b805391a62bbf4f28cbe2b650557c8337
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2019-10-11 19:22:33 +0100

    Added PythonFunctions project

---
+++ PythonFunctions/PythonFunctions.py
@@ -0,0 +1,10 @@
+# Program that will print song lyrics
+
+def repeat_stuff(stuff, num_repeats = 10):
+  return stuff*num_repeats
+  
+lyrics = repeat_stuff("Row ", 3) + "Your Boat. "
+
+song = repeat_stuff(lyrics)
+
+print(song)
