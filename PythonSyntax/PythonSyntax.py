6377cc41b50feb67eddd005fb70891dd44efe805
HaylzRandom <hayley.mcc@gmail.com>
2019-10-11 13:59:21 +0100

    Add project files.

---
+++ PythonSyntax/PythonSyntax.py
@@ -0,0 +1,8 @@
+my_age = 29
+half_my_age = 29 / 2
+greeting = 'Hello'
+name = 'Hayley'
+greeting_with_name = greeting + " " + name
+
+print(greeting_with_name)
+print('My age is ' + str(my_age) + '. Half my age is ' + str(half_my_age) + '.') 
